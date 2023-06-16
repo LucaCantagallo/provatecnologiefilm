d15c1254ef45dfd269f74b522ea8cf1902368684
luca.cantagallo <Luca.cantagallo@dechit.it>
2023-06-16 18:19:47 +0200

    Classi con getter e setter

---
+++ src/main/java/com/allenamento/bean/Film.java
@@ -0,0 +1,70 @@
+package com.allenamento.bean;
+
+public class Film {
+	
+	private String nomeFilm;
+	private boolean visto;
+	
+	private int annoDiUscita;
+	private int voto;
+	private int nomeFilmLinguaOriginale;
+	
+	public Film(String nomeFilm) {
+		this.nomeFilm = nomeFilm;
+	}
+
+	public String getNomeFilm() {
+		return nomeFilm;
+	}
+
+	public void setNomeFilm(String nomeFilm) {
+		this.nomeFilm = nomeFilm;
+	}
+
+	public boolean isVisto() {
+		return visto;
+	}
+
+	public void setVisto(boolean visto) {
+		this.visto = visto;
+	}
+
+	public int getAnnoDiUscita() {
+		return annoDiUscita;
+	}
+
+	public void setAnnoDiUscita(int annoDiUscita) {
+		this.annoDiUscita = annoDiUscita;
+	}
+
+	public int getVoto() {
+		return voto;
+	}
+
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+
+	public int getNomeFilmLinguaOriginale() {
+		return nomeFilmLinguaOriginale;
+	}
+
+	public void setNomeFilmLinguaOriginale(int nomeFilmLinguaOriginale) {
+		this.nomeFilmLinguaOriginale = nomeFilmLinguaOriginale;
+	}
+
+	@Override
+	public String toString() {
+		return "Film [nomeFilm=" + nomeFilm + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
--- test/com/allenamento/test/TestClass.java
+++ test/com/allenamento/test/TestClass.java
@@ -1,9 +1,12 @@
 package com.allenamento.test;
 
+import com.allenamento.bean.Film;
+
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println("Giorno 0");
+		Film film = new Film("Le Ali della Libertà");
+		System.out.println(film.toString());
 
 	}
 
